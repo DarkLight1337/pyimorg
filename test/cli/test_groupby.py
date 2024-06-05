6ccf47fe103b0971b68bf6e12f3aae99eb385970
DarkLight1337 <cyrus.tl.leung@gmail.com>
2024-06-05 09:02:42 +0800

    Fix CLI test for windows

--- test/cli/test_groupby.py
+++ test/cli/test_groupby.py
@@ -3,4 +3,4 @@ import subprocess
 
 def test_help():
     assert subprocess.call(['python', '-m', 'pyimorg', 'groupby', '--help']) == 0
-    assert subprocess.call(['pyimorg', 'groupby', '--help']) == 0
+    assert subprocess.call(['pyimorg', 'groupby', '--help'], shell=True) == 0
--- test/cli/test_root.py
+++ test/cli/test_root.py
@@ -3,4 +3,4 @@ import subprocess
 
 def test_help():
     assert subprocess.call(['python', '-m', 'pyimorg', '--help']) == 0
-    assert subprocess.call(['pyimorg', '--help']) == 0
+    assert subprocess.call(['pyimorg', '--help'], shell=True) == 0
